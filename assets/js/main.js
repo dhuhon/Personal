500c0cd97935ff0ca8dd62c6f40baec937519d24
dhuhon <dhuhon@gmail.com>
2022-11-22 20:57:43 -0500

    Added contact and email javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,3 +56,54 @@ var swiperProject = new Swiper(".projects__container", {
 
       },
   });
+
+
+  /* ========================EMAIL JS====================== */
+
+  const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactMessage = document.getElementById('contact-message'),
+    contactConfirmation = document.getElementById('contact-confirm');
+
+
+    const  sendEmail = async (e) =>{
+
+        try{
+            e.preventDefault();
+            if(contactName.value === '' | contactEmail.value === '' || contactMessage.value === ''){
+                contactConfirmation.classList.remove('color-blue');
+                contactConfirmation.classList.add('color-red');
+    
+    
+                contactConfirmation.textContent = 'Write All the input fields ';
+            }else{
+               let emailresolve = await emailjs.sendForm('service_3hkk3q8','template_udel2re','#contact-form','HIORaJHechUmLC1v_')
+              
+                    contactConfirmation.classList.add('color-blue');
+                    contactConfirmation.textContent = 'Message Sent 👍'
+    
+                    
+    
+    
+                    setTimeout(()=>{
+                        contactConfirmation.textContent=''
+                    },5000);
+
+
+
+                    contactName.value = '';
+                    contactEmail.value= '';
+                    contactMessage.value = '';
+               
+            }
+        }catch(error){
+            alert('OOPS! Something went wrong',error);
+            console.log(error.message);
+        }
+
+        
+
+    }
+
+    contactForm.addEventListener('submit',sendEmail);
